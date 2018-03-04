84a82a4162d6bb06781424c9dce8170991230cdd
Michał Begejowicz <michal.begejowicz@codesthq.com>
2018-03-04 11:51:48 +0100

    Add Model

---
+++ lib/conflow/redis/model.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Conflow
+  module Redis
+    # Models adds .field method which allows to define fields easily.
+    module Model
+      def field(name, type)
+        case type
+        when :hash  then FieldBuilder.new(name, Conflow::Redis::HashField).call(self)
+        when :array then FieldBuilder.new(name, Conflow::Redis::ArrayField).call(self)
+        else raise ArgumentError, "Unknown type: #{type}. Should be one of: [:hash, :array]"
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/conflow/redis/model_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+RSpec.describe Conflow::Redis::Model, redis: true do
+  let(:test_class) do
+    Struct.new(:key) do
+      extend Conflow::Redis::Model
+
+      field :params, :hash
+      field :records, :array
+    end
+  end
+
+  let(:instance) { test_class.new("test_key") }
+
+  it "defines proper methods" do
+    expect(instance).to respond_to(:params, :params=, :records, :records=)
+  end
+
+  context "when type is incorrect" do
+    let(:test_class) do
+      Struct.new(:key) do
+        extend Conflow::Redis::Model
+
+        field :params, :linked_list
+      end
+    end
+
+    it "raises error" do
+      expect { test_class }.to raise_error(ArgumentError, "Unknown type: linked_list. Should be one of: [:hash, :array]")
+    end
+  end
+end
