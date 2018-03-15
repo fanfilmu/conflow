dddfb42294d6ff0d0a3f325f412e900a096cb363
Michał Begejowicz <michal.begejowicz@codesthq.com>
2018-03-15 20:08:33 +0100

    Remove unused ArrayField methods

--- lib/conflow/redis/array_field.rb
+++ lib/conflow/redis/array_field.rb
@@ -6,24 +6,6 @@ module Conflow
     class ArrayField < Field
       include Enumerable
 
-      def [](index)
-        command :lindex, [key, index]
-      end
-
-      def []=(index, value)
-        command :lset, [key, index, value]
-      end
-
-      def insert(value, after: nil, before: nil)
-        if after
-          command :linsert, [key, :after, after, value]
-        elsif before
-          command :linsert, [key, :before, before, value]
-        else
-          raise ArgumentError, "You need to pass one of [:after, :before] keywords"
-        end
-      end
-
       def size
         command :llen, [key]
       end
@@ -32,25 +14,9 @@ module Conflow
         command :lrange, [key, 0, -1]
       end; alias to_ary to_a
 
-      def pop
-        command :rpop, [key]
-      end
-
       def push(*values)
         command :rpush, [key, values]
       end; alias << push
-
-      def concat(ary)
-        push(*ary)
-      end
-
-      def shift
-        command :lpop, [key]
-      end
-
-      def unshift(value)
-        command :lpush, [key, value]
-      end
 
       def overwrite(new_array)
         redis.with do |conn|
--- spec/unit/conflow/redis/array_field_spec.rb
+++ spec/unit/conflow/redis/array_field_spec.rb
@@ -8,39 +8,6 @@ RSpec.describe Conflow::Redis::ArrayField, redis: true do
     it { expect { subject }.to change { array_field.to_a }.to(expected_array) }
   end
 
-  describe "#[]" do
-    subject { array_field[0] }
-
-    it { is_expected.to eq nil }
-
-    context "when array is not empty" do
-      before { array_field << "val" }
-      it { is_expected.to eq "val" }
-    end
-  end
-
-  describe "#[]=" do
-    subject { filled_array[1] = "oy" }
-    it_behaves_like "action changing array", %w[a oy c]
-  end
-
-  describe "#insert" do
-    context "when used with before" do
-      subject { filled_array.insert("something", before: :c) }
-      it_behaves_like "action changing array", %w[a b something c]
-    end
-
-    context "when used with after" do
-      subject { filled_array.insert("something", after: :a) }
-      it_behaves_like "action changing array", %w[a something b c]
-    end
-
-    context "when used without any option" do
-      subject { filled_array.insert("something") }
-      it { expect { subject }.to raise_error(ArgumentError) }
-    end
-  end
-
   describe "#size" do
     subject { filled_array.size }
     it { is_expected.to eq 3 }
@@ -51,31 +18,9 @@ RSpec.describe Conflow::Redis::ArrayField, redis: true do
     it { is_expected.to eq %w[a b c] }
   end
 
-  describe "#pop" do
-    subject { filled_array.pop }
-    it_behaves_like "action changing array", %w[a b]
-    it { is_expected.to eq "c" }
-  end
-
   describe "#push" do
     subject { filled_array.push("d", "e") }
     it_behaves_like "action changing array", %w[a b c d e]
-  end
-
-  describe "#concat" do
-    subject { filled_array.concat %w[d e] }
-    it_behaves_like "action changing array", %w[a b c d e]
-  end
-
-  describe "#shift" do
-    subject { filled_array.shift }
-    it_behaves_like "action changing array", %w[b c]
-    it { is_expected.to eq "a" }
-  end
-
-  describe "#unshift" do
-    subject { filled_array.unshift("d") }
-    it_behaves_like "action changing array", %w[d a b c]
   end
 
   describe "#overwrite" do
